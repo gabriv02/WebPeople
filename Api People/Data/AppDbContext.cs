bb75561b17f07f31fb519e7f0d43980737ee1b02
Gabriela <a2020120887@estudiantes.upsa.edu.bo>
2021-10-13 12:41:04 -0400

    Add project files.

---
+++ Api People/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Api_People.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_People.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ WebPeople/Models/Person.cs
@@ -0,0 +1,26 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace WebPeople.Models
+{
+        public enum SexType
+        {
+            Female = 0,
+            Male = 1,
+        }
+    public class Person
+    {
+        [Key]
+
+        public int PersonId { get; set; }
+        [Required(ErrorMessage ="El nombre es requerido")]
+        [StringLength(60,MinimumLength =5,ErrorMessage =" El nombre debe tener entre 5 a 60 caracteres")]
+        [Display(Name ="Nombre completo de la persona")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name="sexo")]
+        public SexType Sex { get; set; }
+        [Display(Name ="Pasa tiempos")]
+        public string Hobbie { get; set; }
+    }
+}
